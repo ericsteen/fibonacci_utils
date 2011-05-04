69f5b59b387b765a5205c32226949f96f18b61e8
Eric Steen <ericsteen1@gmail.com>
2011-05-04 01:50:39 -0700

    removed .idea/ from git cache

--- lib/fibonacci_utils/fibonacci_range_utils.rb
+++ lib/fibonacci_utils/fibonacci_range_utils.rb
@@ -5,24 +5,23 @@ module FibonacciRangeUtils
   KEY_FIB_LEVELS = [0.236, 0.382, 0.500, 0.618, 0.764, 1.000, 1.382, 1.618, 2.618, 4.236]
 
 # Given a move in either direction(up or down) within a range, computes the value at the closest
-# Fibonacci level out of all key levels.
+# Fibonacci level out of all key levels. Note: returns a rough estimate due to float imprecision.
 #
-# @return [Float] the value at the closest Fibonacci proportion.
+# @params [Float] the value at which one would like to find the closest fib value
+# @return [Float, String] the value at the closest Fibonacci proportion and which proportion it corresponds to.
   def nearest_fibonacci_level(val)
     span = (self.end - self.begin).abs
     move = (val - self.begin).abs
 
     current_level = move/span
-    puts "------------" + current_level.to_s
     nearest_key_level = KEY_FIB_LEVELS.sort_by { |level| (level-current_level).abs }.first
-    puts "###############" + nearest_key_level.to_s
 
     if self.begin < self.end
       nearest_fib_value = self.begin + span*nearest_key_level
     else
       nearest_fib_value = self.begin - span*nearest_key_level
     end
-    nearest_fib_value.round(3)
+    [nearest_fib_value.round(3), nearest_key_level]
   end
 
 end
--- spec/fibonacci_utils_spec.rb
+++ spec/fibonacci_utils_spec.rb
@@ -3,32 +3,30 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "FibonacciRangeUtils" do
 
   context "up move" do
+    it "Range.new(0,1).nearest_fibonacci_level(0.2) => 0.236" do
+      Range.new(0,1).nearest_fibonacci_level(0.2).should == [0.236,0.236]
+    end
     it "Range.new(1,2).nearest_fibonacci_level(1.6) => 1.618" do
-      Range.new(1,2).nearest_fibonacci_level(1.6).should == 1.618
+      Range.new(1,2).nearest_fibonacci_level(1.6).should == [1.618, 0.618]
     end
     it "Range.new(1,3).nearest_fibonacci_level(2.1) => 2" do
-      Range.new(1,3).nearest_fibonacci_level(2.1).should == 2
+      Range.new(1,3).nearest_fibonacci_level(2.1).should == [2, 0.500]
     end
     it "Range.new(4,6).nearest_fibonacci_level(5.7) => 5.528" do
-      Range.new(4,6).nearest_fibonacci_level(5.7).should == 5.528
-    end
-    it "Range.new(0,1).nearest_fibonacci_level(0.2) => 0.236" do
-      Range.new(0,1).nearest_fibonacci_level(0.2).should == 0.236
+      Range.new(4,6).nearest_fibonacci_level(5.7).should == [5.528, 0.764]
     end
     it "Range.new(1,3).nearest_fibonacci_level(1.5) => 0.472" do
-      Range.new(1,3).nearest_fibonacci_level(1.5).should == 1.472
+      Range.new(1,3).nearest_fibonacci_level(1.5).should == [1.472,0.236]
     end
 
   end
 
   context "down move" do
     it "Range.new(0.5,0).nearest_fibonacci_level(0.2) => 0.191" do
-      Range.new(0.5,0).nearest_fibonacci_level(0.2).should == 0.191
+      Range.new(0.5,0).nearest_fibonacci_level(0.2).should == [0.191,0.618]
     end
     it "Range.new(165.54, 50.44).nearest_fibonacci_level(110) => 120" do
-      Range.new(165.54, 50.44).nearest_fibonacci_level(110).should == 120
+      Range.new(165.54, 50.44).nearest_fibonacci_level(110).should == [107.99,0.500]
     end
-
-
   end
 end
