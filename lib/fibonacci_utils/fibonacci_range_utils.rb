71dd26ede339813130e8b703ea5f8db1995eccaf
Eric Steen <ericsteen1@gmail.com>
2011-05-05 20:42:24 -0700

    more specs, added ValueOutOfRangeError < ArgumentError

--- lib/fibonacci_utils/fibonacci_range_utils.rb
+++ lib/fibonacci_utils/fibonacci_range_utils.rb
@@ -1,18 +1,26 @@
-require 'bigdecimal'
+#TODO: require 'bigdecimal'
 
 module FibonacciRangeUtils
 
   KEY_FIB_LEVELS = [0.236, 0.382, 0.500, 0.618, 0.764, 1.000, 1.382, 1.618, 2.618, 4.236]
 
+  class ValueOutOfRangeError < ArgumentError; end
+
 # Given a move in either direction(up or down) within a range, computes the value at the
-# Fibonacci level nearest to value passed as a parameter from the given key levels.
+# Fibonacci level nearest to the value passed as a parameter from the given key levels.
 # Note: returns a rough estimate due to float imprecision.
 #
 # This method can be used to find the expected extent of a move or retracement of a financial instrument.
 #
 # @param [Float], the value at which one would like to find the nearest fib level value
-# @return [Float, String] the value at the closest Fibonacci proportion and which proportion it corresponds to.
+# @return [Float, String] the value at the closest Fibonacci proportion and the corresponding proportion.
   def nearest_fibonacci_level(val)
+    if val < self.begin
+      raise FibonacciRangeUtils::ValueOutOfRangeError, "Must pass a value within the range." unless val > self.end
+    elsif val > self.begin
+      raise FibonacciRangeUtils::ValueOutOfRangeError, "Must pass a value within the range." unless val < self.end
+    end
+    
     span = (self.end - self.begin).abs
     move = (val - self.begin).abs
 
